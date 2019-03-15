2e26ccd0f00d861534b355027c8d8996388e0772
Luca Tavernini <tavernoshca@gmail.com>
2019-03-15 12:34:14 +0100

    Design signup

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,105 +1,121 @@
 // @flow
 
 import React from "react";
-import { Redirect } from "react-router-dom";
+import {Redirect, Link} from "react-router-dom";
 
-import { signup } from "../api";
+import {Header, Container, Form, Button} from 'semantic-ui-react'
+
+import {signup} from "../api";
 
 type Props = {};
 
 type State = {
-  login: string,
-  firstname: string,
-  lastname: string,
-  password: string,
-  error: ?Error,
-  redirectToReferrer: boolean
+    login: string,
+    firstname: string,
+    lastname: string,
+    password: string,
+    error: ?Error,
+    redirectToReferrer: boolean
 };
 
 class Signup extends React.Component<Props, State> {
-  state = {
-    login: "",
-    firstname: "",
-    lastname: "",
-    password: "",
-    error: null,
-    redirectToReferrer: false
-  };
+    state = {
+        login: "",
+        firstname: "",
+        lastname: "",
+        password: "",
+        error: null,
+        redirectToReferrer: false
+    };
 
-  handleLoginChanged = (event: Event) => {
-    if (event.target instanceof HTMLInputElement) {
-      this.setState({ login: event.target.value });
-    }
-  };
+    handleLoginChanged = (event: Event) => {
+        if (event.target instanceof HTMLInputElement) {
+            this.setState({login: event.target.value});
+        }
+    };
 
-  handleFirstNameChanged = (event: Event) => {
-    if (event.target instanceof HTMLInputElement) {
-      this.setState({ firstname: event.target.value });
-    }
-  };
+    handleFirstNameChanged = (event: Event) => {
+        if (event.target instanceof HTMLInputElement) {
+            this.setState({firstname: event.target.value});
+        }
+    };
 
-  handleLastNameChanged = (event: Event) => {
-    if (event.target instanceof HTMLInputElement) {
-      this.setState({ lastname: event.target.value });
-    }
-  };
+    handleLastNameChanged = (event: Event) => {
+        if (event.target instanceof HTMLInputElement) {
+            this.setState({lastname: event.target.value});
+        }
+    };
 
-  handlePasswordChanged = (event: Event) => {
-    if (event.target instanceof HTMLInputElement) {
-      this.setState({ password: event.target.value });
-    }
-  };
+    handlePasswordChanged = (event: Event) => {
+        if (event.target instanceof HTMLInputElement) {
+            this.setState({password: event.target.value});
+        }
+    };
 
-  handleSubmit = (event: Event) => {
-    event.preventDefault();
-    const { login, firstname, lastname, password } = this.state;
-    signup(login, firstname, lastname, password)
-      .then(result => {
-        console.log("Signup result ", result);
-        this.setState({ redirectToReferrer: true, error: null });
-      })
-      .catch(error => this.setState({ error }));
-  };
+    handleSubmit = (event: Event) => {
+        event.preventDefault();
+        const {login, firstname, lastname, password} = this.state;
+        signup(login, firstname, lastname, password)
+            .then(result => {
+                console.log("Signup result ", result);
+                this.setState({redirectToReferrer: true, error: null});
+            })
+            .catch(error => this.setState({error}));
+    };
 
-  render() {
-    const { redirectToReferrer, error } = this.state;
-
-    if (redirectToReferrer) {
-      return <Redirect to="/login" />;
+    handleCancel = (event: Event) => {
+        event.preventDefault();
+        this.setState({redirectToReferrer: true, error: null});
     }
 
-    return (
-      <div>
-        <h1>Bank of Rapperswil</h1>
-        <form>
-          <h2>Registrieren</h2>
-          <input
-            onChange={this.handleLoginChanged}
-            placeholder="Login"
-            value={this.state.login}
-          />
-          <input
-            onChange={this.handleFirstNameChanged}
-            placeholder="Vorname"
-            value={this.state.firstname}
-          />
-          <input
-            onChange={this.handleLastNameChanged}
-            placeholder="Nachname"
-            value={this.state.lastname}
-          />
-          <input
-            onChange={this.handlePasswordChanged}
-            placeholder="Passwort"
-            type="password"
-            value={this.state.password}
-          />
-          <button onClick={this.handleSubmit}>Account eröffnen</button>
-        </form>
-        {error && <p>Es ist ein Fehler aufgetreten!</p>}
-      </div>
-    );
-  }
+    render() {
+        const {redirectToReferrer, error} = this.state;
+
+        if (redirectToReferrer) {
+            return <Redirect to="/login"/>;
+        }
+
+        return (
+            <Container style={{width: '40%'}}>
+                <Header as='h1'>Bank of Rapperswil</Header>
+                <Form>
+                    <Header as='h2'>Registrieren</Header>
+                    <Form.Field>
+                        <input
+                            onChange={this.handleLoginChanged}
+                            placeholder="Login"
+                            value={this.state.login}
+                        />
+                    </Form.Field>
+                    <Form.Field>
+                        <input
+                            onChange={this.handleFirstNameChanged}
+                            placeholder="Vorname"
+                            value={this.state.firstname}
+                        />
+                    </Form.Field>
+                    <Form.Field>
+                        <input
+                            onChange={this.handleLastNameChanged}
+                            placeholder="Nachname"
+                            value={this.state.lastname}
+                        />
+                    </Form.Field>
+                    <Form.Field>
+                        <input
+                            onChange={this.handlePasswordChanged}
+                            placeholder="Passwort"
+                            type="password"
+                            value={this.state.password}
+                        />
+                    </Form.Field>
+                    <Button primary onClick={this.handleSubmit}>Account eröffnen</Button>
+                    <Button secondary onClick={this.handleCancel}>Abbrechen</Button>
+                </Form>
+                {error && <p>Es ist ein Fehler aufgetreten!</p>}
+            </Container>
+        );
+    }
 }
 
 export default Signup;
